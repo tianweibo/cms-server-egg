79e3eedb8518afb3092c017eef427d8a71e13814
yinlei <yinlei1126@qq.com>
2021-02-18 16:25:23 +0800

    feat: 🎸 增加db.sql

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS buried_points_server DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` int(11) NOT NULL DEFAULT '0',
+  `updated_at` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8;

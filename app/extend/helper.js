1f0f455dd9d292216cdea6094c1fb0f4507a9989
lei.yin <yinlei1126@qq.com>
2021-04-02 11:32:38 +0800

    test: 💍 test

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -4,4 +4,4 @@
 const moment = require('moment');
 exports.formatDate = time => moment(new Date(time * 1000)).fromNow();
 
-exports.apiResponse = (code=0, msg='success', data=[]) => ({ code, msg, data });
+exports.apiResponse = (code=0, msg='success', data={}) => ({ code, msg, data });

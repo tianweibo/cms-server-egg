837b36979d6a9aa6fdfb4ab293104df5c3f9aa98
lei.yin <yinlei1126@qq.com>
2021-04-02 09:21:42 +0800

    test: 💍 完善报表

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -4,7 +4,6 @@
 import 'egg';
 import ExportActivity = require('../../../app/controller/activity');
 import ExportHome = require('../../../app/controller/home');
-import ExportOldReport = require('../../../app/controller/old-report');
 import ExportProject = require('../../../app/controller/project');
 import ExportReport = require('../../../app/controller/report');
 import ExportSysTag = require('../../../app/controller/sysTag');
@@ -13,7 +12,6 @@ declare module 'egg' {
   interface IController {
     activity: ExportActivity;
     home: ExportHome;
-    oldReport: ExportOldReport;
     project: ExportProject;
     report: ExportReport;
     sysTag: ExportSysTag;

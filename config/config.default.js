1499a90cd1eccb5fbd8659cb945a2a353daa2ae9
lei.yin <yinlei1126@qq.com>
2021-04-06 15:20:14 +0800

    test: 💍 完善 config.prod.js & start:docker

--- config/config.default.js
+++ config/config.default.js
@@ -12,25 +12,25 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
-  config.sequelize = {
-    dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
-    database: 'buried_points_server',
-    host: 'rm-k2jq4d1297g80t326.mysql.zhangbei.rds.aliyuncs.com',
-    port: 3306,
-    username: 'prod',
-    password: 'Prodc959ed5e61ce4451803Enbrands',
-    delegate: 'model',
-    baseDir: 'model',
-    define: {
-      // raw: true,
-      underscored: true,
-      freezeTableName: true, //直接查找设置的表名，默认是表名加s或者es
-      timestamps: false,
-      createdAt: "CreatedAt",  //自定义时间戳
-      updatedAt: "UpdatedAt", // 自定义时间戳
-      timezone: '+08:00' // 保存为本地时区
-    }
-  };
+  // config.sequelize = {
+  //   dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
+  //   database: 'buried_points_server',
+  //   host: 'rm-k2jq4d1297g80t326.mysql.zhangbei.rds.aliyuncs.com',
+  //   port: 3306,
+  //   username: 'prod',
+  //   password: 'Prodc959ed5e61ce4451803Enbrands',
+  //   delegate: 'model',
+  //   baseDir: 'model',
+  //   define: {
+  //     // raw: true,
+  //     underscored: true,
+  //     freezeTableName: true, //直接查找设置的表名，默认是表名加s或者es
+  //     timestamps: false,
+  //     createdAt: "CreatedAt",  //自定义时间戳
+  //     updatedAt: "UpdatedAt", // 自定义时间戳
+  //     timezone: '+08:00' // 保存为本地时区
+  //   }
+  // };
 
   // config.mysql = {
   //   // 单数据库信息配置

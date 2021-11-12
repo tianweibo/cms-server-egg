e4e49fbded72964a8401f50821974fb7596137bf
tianweibo <839445383@qq.com>
2021-11-12 10:20:14 +0800

    事件确认

--- config/config.default.js
+++ config/config.default.js
@@ -34,7 +34,6 @@ module.exports = appInfo => {
       timestamps: false,
       createdAt: "CreatedAt",  //自定义时间戳
       updatedAt: "UpdatedAt", // 自定义时间戳
-      timezone: '+08:00' // 保存为本地时区
     }
   };
   config.jwt={
